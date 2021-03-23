63e130147eb45bf50f3225bbc7eeab52de4502d2
Kalata-22 <80474953+Kalata-22@users.noreply.github.com>
2021-03-23 20:19:13 +0200

    Add files via upload

---
+++ Cars.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+//using CRUD_Operations.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace AdminPanelTutorial
+{
+    public class CarsController : Controller
+        
+    {
+       // MyDbContext db = new MyDbContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Cars.ToList()); 
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateCars(Cars cars)
+        {
+            db.Cars.Add(cars);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Cars");
+        }
+        [HttpPost]
+        public bool Delete(int id)
+        {
+            try
+            {
+                Cars cars = db.Cars.Where(s => s.Id == id).First();
+                db.Cars.Remove(cars);
+                db.SaveChanges();
+                return true;
+            }
+            catch (System.Exception)
+            {
+                return false;
+            }
+
+        }
+        public ActionResult Update(int id)
+        {
+            return View(db.Cars.Where(s => s.Id == id).First());
+        }
+        [HttpPost]
+        public ActionResult UpdateCars(Cars cars)
+        {
+            Cars d = db.Cars.Where(s => s.Id == cars.Id).First();
+            d.Name = cars.Name;
+            d.DateOfCreation = cars.DateOfCreation;
+            d.EngineType = cars.EngineType;
+            d.HorsePower = cars.HorsePower;
+            d.Transmission = cars.Transmission ;
+            d.Category = cars.Category;
+            d.Mileage = cars.Mileage;
+            d.Color = cars.Color;
+
+            db.SaveChanges();
+            return RedirectToAction("Index", "Cars");
+        }
+    }
+}
---
+++ MyDbContext.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CRUD_Operations.Models
+    
+{
+    public partial class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Admins> Admins { get; set; }
+        public virtual DbSet<Cars> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=12345;database=hospital_db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Admins>(entity =>
+            {
+                entity.ToTable("admins");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Created)
+                    .HasColumnName("created")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasColumnName("password")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasColumnName("phone")
+                    .HasMaxLength(15)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Cars>(entity =>
+            {
+                entity.ToTable("cars");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.Name)
+                    .HasColumnName("name")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.Property(e => e.DateOfCreation)
+                    .IsRequired()
+                    .HasColumnName("dateofcreation")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EngineType)
+                    .HasColumnName("enginetype")
+                    .HasColumnType("int(11)");
+
+                entity.Property(e => e.HorsePower)
+                    .IsRequired()
+                    .HasColumnName("horsepower")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Transmission)
+                    .IsRequired()
+                    .HasColumnName("transmission")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Category)
+                    .IsRequired()
+                    .HasColumnName("category")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Mileage)
+                    .IsRequired()
+                    .HasColumnName("mileage")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Color)
+                    .IsRequired()
+                    .HasColumnName("color")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+           
+
+           
+            
+        }
+    }
+}
